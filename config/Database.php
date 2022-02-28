7abcda337545f4d7933fb3dc2b2d72bc41c4ae86
Brianna Watson <bdwatson@mail.fhsu.edu>
2022-02-28 21:31:26 +0000

    trying a change

--- config/Database.php
+++ config/Database.php
@@ -3,14 +3,14 @@
         private $host = 'j21q532mu148i8ms.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
         private $db_name = 'o9jmvxv9m4v59udl';
         private $username = 'ezkkogqie6ebtp32';
-        private $my_env_password = getenv('DATA_PASS');
+        //private $my_env_password = getenv('DATA_PASS');
         private $conn;
 
         public function connect() {
             $this->conn = null;
 
             try {
-                $this->conn = new PDO('mysql:host='.$this->host.';dbname='.$this->db_name, $this->username, $this->my_env_password); 
+                $this->conn = new PDO('mysql:host='.$this->host.';dbname='.$this->db_name, $this->username, getenv('DATA_PASS')); 
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (PDOException $e) {
                 $error_message = 'Database Error: ';
